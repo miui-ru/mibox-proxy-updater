58d2879176f69e51f1e385c2d7ab8f2a92afdbaa
Yaodong Zhao <yaodong.zhao@outlook.com>
2016-07-15 23:54:05 -0600

    Unique proxy by IP

--- updater.py
+++ updater.py
@@ -4,7 +4,6 @@ import re
 from os import path
 from requests import Timeout
 
-
 LOCAL_PROXY_FILE = path.join(path.dirname(__file__), 'proxy.json')
 GIMME_PROXY_API = 'http://gimmeproxy.com/api/getProxy?country=CN&protocol=http&anonymityLevel=1'
 INCLOAK_PROXY_PAGE = 'https://incloak.com/proxy-list/?country=CN&anon=34'
@@ -25,7 +24,7 @@ def request_proxy():
 def test_proxy(proxy):
     try:
         print('testing %s' % proxy)
-        response = requests.get('http://baidu.com', timeout=2, proxies={'http': 'http://%s' % proxy})
+        response = requests.get('http://baidu.com', timeout=1, proxies={'http': 'http://%s' % proxy})
         return re.search('www\.baidu\.com', response.text) is not None
     except Timeout:
         return False
@@ -64,20 +63,35 @@ def filter_broken_proxy(proxy_list):
 
 
 def update_squid_conf(proxy_list):
-    lines = ['cache_peer {} parent {} 0 round-robin no-query no-digest'.format(*proxy.split(':')[0:2]) for proxy in proxy_list]
+    lines = ['cache_peer {} parent {} 0 round-robin no-query no-digest'.format(*proxy.split(':')[0:2]) for proxy in
+             proxy_list]
     with open('proxy_server_list.conf', 'w') as f:
         f.write('\n'.join(lines))
+
+
+def unique_by_ip(proxy_list):
+    filtered_list = []
+    ip_list = []
+    for proxy in proxy_list:
+        ip = proxy.split(':')[0]
+        if ip in ip_list:
+            continue
+        else:
+            ip_list.append(ip)
+            filtered_list.append(proxy)
+    return filtered_list
 
 
 def main():
     proxy_list = read_local_list()
     proxy_list = filter_broken_proxy(proxy_list)
     proxy_list += collect_proxy_addresses()
-    proxy_list = list(set(proxy_list))
+    proxy_list = unique_by_ip(proxy_list)
 
     print('current proxy count: %i' % len(proxy_list))
     save_local_list(proxy_list)
     update_squid_conf(proxy_list)
+
 
 if __name__ == '__main__':
     main()
